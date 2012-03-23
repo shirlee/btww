23b457f22ed076338099e1bb4caf9c6161161813
Shirlee Berman <shirlee.berman@gmail.com>
2012-03-23 16:08:19 -0500

    uncommented oauth initializer

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
-
-
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET']
 
 end
